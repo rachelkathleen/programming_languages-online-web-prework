41db3a9b02eeeffb453ca487915370b9fada7177
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-13 21:25:35 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,5 +2,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language|
     langages.each do |type, string|
+    end
+  end
     new_hash[language] = [type, style]
 end
