7226955901b11cc278f3be10042f04a944e433d0
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-13 21:41:10 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-  # your code here
+  solution_hash = {}
+  languages.each do |type, hash|
+    hash.each do |name, string_value|
+      solution_hash[name] ||= string_value
+      solution_hash[name][:style] ||= []
+      solution_hash[name][:style] << type
+    end
+  end
+  solution_hash
 end
