ae160177d5008b80fbd9bbf8382e25e224e31253
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-13 21:34:41 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,24 +2,10 @@ def reformat_languages(languages)
   solution_hash = {}
   languages.each do |type, hash|
     hash.each do |name, string_value|
-
-    solution_hash[name] || = string_value
-    solution_hash[name][:style] ||= []
-    solution_hash[name][:style] << type
+      solution_hash[name] || = string_value
+      solution_hash[name][:style] ||= []
+      solution_hash[name][:style] << type
     end
   end
   solution_hash
 end
-
-
-# def reformat_languages(languages)
-#   new_languages = {}
-#   languages.each do |type, hash|
-#     hash.each do |name, attributes|
-#       new_languages[name] ||= attributes
-#       new_languages[name][:style] ||= []
-#       new_languages[name][:style] << type
-#     end
-#   end
-#   new_languages
-# end
